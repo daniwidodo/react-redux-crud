8928d17d04738d089fb4fa7868d091b9de54fa1d
daniwidodo <dani.widodo87@gmail.com>
2022-06-06 08:37:04 +0800

    initial install

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import Test from "./components/pages/Test";
+import { PokemonApiBaseurl } from "./server/PokemonApi";
+import axios from "axios";
+import { Button, Card, Input } from "@material-ui/core";
+import "./styles/PokemonCard.css";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const [pokemons, setPokemons] = React.useState([]);
+
+  useEffect(() => {
+    // console.log('wohoo!', PokemonApiBaseurl);
+    // pokemonHandler();
+
+    axios
+      .get(PokemonApiBaseurl)
+      .then((res) => {
+        const listPokemons = res.data.results;
+        setPokemons(listPokemons);
+        console.log(listPokemons);
+      })
+      .catch((e) => e);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Test />
+      <div>
+        <h1> Counter : {count}</h1>
+        <Button
+          onClick={() => setCount(count + 1)}
+          variant="contained"
+          color="primary"
         >
-          Learn React
-        </a>
-      </header>
+          increment
+        </Button>
+        <Button
+          onClick={() => setCount(count - 1)}
+          variant="contained"
+          color="secondary"
+        >
+          decrement
+        </Button>
+      </div>
+
+      <Input
+        style={{
+          width: "90%",
+          margin: "10px",
+        }}
+      ></Input>
+
+      <div className="columns">
+        {pokemons.map((pokemon: any) => (
+          <Card className="card" key={pokemon.name}>
+            <p>{pokemon.name}</p>
+            <img src="" alt="" />
+            <Button
+              onClick={() => setCount(count - 1)}
+              variant="contained"
+              color="inherit"
+            >
+              Add to favorites
+            </Button>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
